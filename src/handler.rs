0c97be947f04b3f77a1e560c2d771e87a256ac94
yoshihitoh <yoshihito.arih@gmail.com>
2022-11-17 10:57:15 +0900

    feat(cli): support `AWS_SESSION_EXPIRATION` environment variable.

--- src/handler.rs
+++ src/handler.rs
@@ -14,22 +14,23 @@ pub trait HandleCredentials {
 
 struct Variable<'a> {
     name: &'a str,
-    value: Option<&'a str>,
+    value: Option<String>,
 }
 
 fn into_variables(request: HandleCredentialsRequest) -> Vec<Variable> {
-    fn v<'a>(name: &'a str, value: Option<&'a str>) -> Variable<'a> {
-        Variable { name, value }
+    fn v<S: Into<String>>(name: &str, value: Option<S>) -> Variable {
+        Variable { name, value: value.map(|s| s.into()) }
     }
 
     vec![
         // for AWS SDK, aws-cli
-        v("AWS_PROFILE", None),
+        v("AWS_PROFILE", Option::<String>::None),
         v("AWS_REGION", Some(request.region_name)),
         v("AWS_DEFAULT_REGION", Some(request.region_name)),
         v("AWS_ACCESS_KEY_ID", Some(request.credentials.key())),
         v("AWS_SECRET_ACCESS_KEY", Some(request.credentials.secret())),
         v("AWS_SESSION_TOKEN", request.credentials.token()),
+        v("AWS_SESSION_EXPIRATION", request.credentials.expires_at.map(|dt| dt.to_rfc3339())),
         // for prompts
         v("ASSUME_ROLERS_PROFILE", Some(request.profile_name)),
     ]
